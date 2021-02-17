b4fa75e9e0e5dc6296b1c3b86259619f90d221cb
Pavel Lezin <pavellezin@yandex.ru>
2021-02-17 17:37:15 +0500

    initial commit

---
+++ auth-service/src/test/java/pro/paullezin/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.paullezin.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
